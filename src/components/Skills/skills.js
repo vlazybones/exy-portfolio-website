3ec29386736d388394af4f0251b97af41d27e0a0
Eylin <eylinxinyu.booi@stengg.com>
2023-11-29 14:46:49 +0800

    updated links for skills page

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -24,17 +24,83 @@ function Skills() {
       <span className="skillDesc">I have had the opportunity to immerse myself in a diverse array of programming languages. </span>
       <div className="skillBars">
         <div className="skillBar">
-          <HoverableIcon icon={<Html5OriginalWordmark />} defaultSize="70" />
-          <HoverableIcon icon={<Css3OriginalWordmark />} defaultSize="70" />
-          <HoverableIcon icon={<JavascriptPlain />} defaultSize="70" />
-          <HoverableIcon icon={<NodejsOriginal />} defaultSize="70" />
-          <HoverableIcon icon={<JqueryOriginalWordmark />} defaultSize="70" />
-          <HoverableIcon icon={<MysqlOriginalWordmark />} defaultSize="70" />
-          <HoverableIcon icon={<JavaOriginalWordmark />} defaultSize="70" />
-          <HoverableIcon icon={<SpringOriginalWordmark />} defaultSize="70" />
-          <HoverableIcon icon={<ReactOriginalWordmark />} defaultSize="70" />
-          <HoverableIcon icon={<CsharpPlain />} defaultSize="70" />
-          <HoverableIcon icon={<PythonOriginalWordmark />} defaultSize="70" />
+        <a
+        href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<Html5OriginalWordmark />} defaultSize="70" />
+        </a>
+        <a
+        href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<Css3OriginalWordmark />} defaultSize="70" />
+        </a>
+        <a
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<JavascriptPlain />} defaultSize="70" />
+        </a>
+        <a
+        href="https://developer.mozilla.org/en-US/docs/Glossary/Node.js"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<NodejsOriginal />} defaultSize="70" />
+        </a>
+        <a
+        href="https://jquery.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<JqueryOriginalWordmark />} defaultSize="70" />
+        </a>
+        <a
+        href="https://www.oracle.com/mysql/what-is-mysql/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<MysqlOriginalWordmark />} defaultSize="70" />
+        </a>
+        <a
+        href="https://www.ibm.com/topics/java"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<JavaOriginalWordmark />} defaultSize="70" />
+        </a>
+        <a
+        href="https://spring.io/projects/spring-boot"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<SpringOriginalWordmark />} defaultSize="70" />
+        </a>
+        <a
+        href="https://legacy.reactjs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<ReactOriginalWordmark />} defaultSize="70" />
+        </a>
+        <a
+        href="https://learn.microsoft.com/en-us/dotnet/csharp/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<CsharpPlain />} defaultSize="70" />
+        </a>
+        <a
+        href="https://www.python.org/about/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <HoverableIcon icon={<PythonOriginalWordmark />} defaultSize="70" />
+        </a>
         </div>
       </div>
       </section>
